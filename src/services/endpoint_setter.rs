c98deac2de50dae62cba767c3a55402625da2924
cryptokage2306 <26vivek06@gmail.com>
2025-09-24 18:25:01 +0530

    Refactor endpoint setter to trim quotes and parse URL
    - Updated the set_endpoint function to trim quotes from the input string.
    - Added URL parsing to ensure the endpoint is a valid URL before setting it in HOST_NAME.

--- src/services/endpoint_setter.rs
+++ src/services/endpoint_setter.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 
+use url::Url;
+
 use crate::config::HOST_NAME;
 
 pub fn set_endpoint(endpoint: &str) -> Result<(), Box<dyn Error>> {
@@ -8,6 +10,8 @@ pub fn set_endpoint(endpoint: &str) -> Result<(), Box<dyn Error>> {
     if old_endpoint.is_some() {
         return Err("Endpoint already set".into());
     }
-    HOST_NAME.set(endpoint.to_string()).unwrap();
+    let trimmed = endpoint.trim_matches('"');
+    let uri = Url::parse(trimmed)?;
+    HOST_NAME.set(uri.to_string()).unwrap();
     Ok(())
 }
